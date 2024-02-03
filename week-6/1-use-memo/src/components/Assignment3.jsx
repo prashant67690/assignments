d93632872d9baea93d6d4f8f65e9dd3759baadb1
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-02-03 18:16:51 +0530

    week-6 done

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,27 +1,62 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+  const [items, setItems] = useState([
+    { name: "Chocolates", value: 10 },
+    { name: "Chips", value: 20 },
+    { name: "Onion", value: 30 },
+    { name: "Tomato", value: 30 },
+    // Add more items as needed
+  ]);
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+
+  const onSubmitHandler = (e) => {
+    if (name == "") {
+      return;
+    }
+    setItems([...items, { name, value: price }]);
+  };
+
+  // Your code starts here
+  const totalValue = useMemo(() => {
+    let res = 0;
+    items.forEach((element) => {
+      res = res + element.value;
+    });
+    return res;
+  }, [items]);
+  // Your code ends here
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="Name of The Item"
+        />
+        <input
+          type="text"
+          onChange={(e) => {
+            setPrice(Number(e.target.value));
+          }}
+          placeholder="Price"
+        />
+        <button onClick={onSubmitHandler}>Add Item</button>
+      </div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.name} - Price: ${item.value}
+          </li>
+        ))}
+      </ul>
+      <p>Total Value: {totalValue}</p>
+    </div>
+  );
 };
